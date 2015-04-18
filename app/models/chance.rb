e421b5d6562bfe872f622e566fad3b2df1110349
Daviddsr <daviddsrperiodismo@gmail.com>
2015-04-18 09:32:20 +0200

    Add field email for new chance

--- app/models/chance.rb
+++ app/models/chance.rb
@@ -1,10 +1,12 @@
 class Chance
 	include Mongoid::Document
 	has_many :products
+	has_and_belongs_to_many :tags
 
 	accepts_nested_attributes_for :products
 
 	field :title, type: String
 	field :address, type: String
 	field :schedule, type: String
+	field :email, type: String
 end
